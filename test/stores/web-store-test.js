50c7507e75dcf4c1eb138c11930db0ffaa1c9dd3
Itay Weinberger <itay@joo.la>
2013-09-23 16:11:05 +0300

    Added gists for tests and malformed test

--- test/stores/web-store-test.js
+++ test/stores/web-store-test.js
@@ -16,7 +16,7 @@ vows.describe('nconf/stores/web').addBatch({
   "When using the nconf web store": {
     "with a valid JSON file": {
       topic: function () {
-        this.store = store = new nconf.Web({ web: 'http://localhost:40001/conf/joola.io.sdk' });
+        this.store = store = new nconf.Web({ web: 'https://gist.github.com/itayw/6670060/raw/d5f060ebc8be7e8f612d894e7ae175ff165b7096/store.json' });
         return null;
       },
       "the load() method": {
@@ -26,6 +26,21 @@ vows.describe('nconf/stores/web').addBatch({
         "should load the data correctly": function (err, data) {
           assert.isNull(err);
           assert.deepEqual(data, this.store.store);
+        }
+      }
+    },
+    "with a malformed JSON file": {
+      topic: function () {
+        this.store = store = new nconf.Web({ web: 'https://gist.github.com/itayw/6670060/raw/fffa15574b0370af6d99460438856e386a161cbb/malformed.json#' });
+        return null;
+      },
+      "the load() method with a malformed JSON config file": {
+        topic: function () {
+          this.store.load(this.callback.bind(null, null));
+        },
+        "should respond with an error and indicate file name": function (_, err) {
+          assert.isTrue(!!err);
+          assert.match(err, /malformed\.json/);
         }
       }
     }
